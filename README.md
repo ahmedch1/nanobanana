753afe3a68b14964e522e18d1a3e3dfb1172f9ea
Ahmed Chouihi <ahmedchouihi@gmail.com>
2025-09-06 21:32:20 +0100

    Image Generation from Text

---
+++ README.md
@@ -0,0 +1,2 @@
+## Check This tutorial
+### https://dev.to/googleai/how-to-build-with-nano-banana-complete-developer-tutorial-646
---
+++ main.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from google import genai
+from PIL import Image
+from io import BytesIO
+
+# Configure the client with your API key
+client = genai.Client()
+
+prompt = """Create a photorealistic image of an orange cat
+with a green eyes, sitting on a couch."""
+
+# Call the API to generate content
+response = client.models.generate_content(
+    model="models/gemini-2.5-flash-image-preview",
+    contents=prompt,
+)
+
+# The response can contain both text and image data.
+# Iterate through the parts to find and save the image.
+for part in response.candidates[0].content.parts:
+    if part.text is not None:
+        print(part.text)
+    elif part.inline_data is not None:
+        image = Image.open(BytesIO(part.inline_data.data))
+        image.save("cat.png")
