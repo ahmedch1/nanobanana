4ad076109338afee778b442d70ba0dc53a28c116
Ahmed Chouihi <ahmedchouihi@gmail.com>
2025-09-06 21:38:15 +0100

    Image Editing

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ from io import BytesIO
 # Configure the client with your API key
 client = genai.Client()
 
-prompt = """Create a photorealistic image of an orange cat
-with a green eyes, sitting on a couch."""
+prompt = """Using the image of the cat, create a photorealistic,
+street-level view of the cat walking along a sidewalk in a
+New York City neighborhood, with the blurred legs of pedestrians
+and yellow cabs passing by in the background."""
+
+image=Image.open("cat.png")
 
 # Call the API to generate content
 response = client.models.generate_content(
     model="models/gemini-2.5-flash-image-preview",
-    contents=prompt,
+    contents=[prompt,image]
 )
 
 # The response can contain both text and image data.
@@ -24,4 +28,4 @@ for part in response.candidates[0].content.parts:
         print(part.text)
     elif part.inline_data is not None:
         image = Image.open(BytesIO(part.inline_data.data))
-        image.save("cat.png")
+        image.save("cat2.png")
